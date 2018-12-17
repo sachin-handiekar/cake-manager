33d9023b20d7f4ea8e8d64a2b07ae322db801882
Sachin Handiekar <sach21@gmail.com>
2018-12-17 21:52:43 +0000

    Updated code

--- src/main/java/com/waracle/cakemanager/domain/CakeEntity.java
+++ src/main/java/com/waracle/cakemanager/domain/CakeEntity.java
@@ -1,6 +1,7 @@
 package com.waracle.cakemanager.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
@@ -9,9 +10,8 @@ import java.io.Serializable;
 @Entity
 @Table(name = "Cake", uniqueConstraints = {@UniqueConstraint(columnNames = "ID")})
 @EntityListeners(AuditingEntityListener.class)
+@Data
 public class CakeEntity implements Serializable {
-
-    private static final long serialVersionUID = -1798070786993154676L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -30,41 +30,5 @@ public class CakeEntity implements Serializable {
     @JsonProperty("image")
     private String image;
 
-    public String getTitle() {
-        return title;
-    }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public Integer getCakeId() {
-        return cakeId;
-    }
-
-    @Override
-    public String toString() {
-        return "CakeEntity{" +
-                "cakeId=" + cakeId +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", image='" + image + '\'' +
-                '}';
-    }
 }
